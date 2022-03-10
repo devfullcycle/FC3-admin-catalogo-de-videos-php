6646fb313011758dcd11eb80bdea4d6fe6e1cb1e
Carlos Ferreira <carlos@especializati.com.br>
2022-03-10 19:17:50 +0000

    Test Integration Update Category Use Case

---
+++ tests/Feature/Core/UseCase/Category/UpdateCategoryUseCaseTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Category;
+
+use App\Models\Category as Model;
+use App\Repositories\Eloquent\CategoryEloquentRepository;
+use Core\UseCase\Category\UpdateCategoryUseCase;
+use Core\UseCase\DTO\Category\UpdateCategory\CategoryUpdateInputDto;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UpdateCategoryUseCaseTest extends TestCase
+{
+    public function test_update()
+    {
+        $categoryDb = Model::factory()->create();
+
+        $repository = new CategoryEloquentRepository(new Model());
+        $useCase = new UpdateCategoryUseCase($repository);
+        $responseUseCase = $useCase->execute(
+            new CategoryUpdateInputDto(
+                id: $categoryDb->id,
+                name: 'name updated',
+            )
+        );
+
+        $this->assertEquals('name updated', $responseUseCase->name);
+        $this->assertEquals($categoryDb->description, $responseUseCase->description);
+
+        $this->assertDatabaseHas('categories', [
+            'name' => $responseUseCase->name,
+        ]);
+    }
+}
