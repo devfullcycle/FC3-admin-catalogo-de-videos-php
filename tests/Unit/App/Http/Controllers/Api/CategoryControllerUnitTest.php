cdde3c989092a8fdbeba429a8302f275a49c2f16
Carlos Ferreira <carlos@especializati.com.br>
2022-03-17 12:56:01 +0000

    Fixbug test - total params dto

--- tests/Unit/App/Http/Controllers/Api/CategoryControllerUnitTest.php
+++ tests/Unit/App/Http/Controllers/Api/CategoryControllerUnitTest.php
@@ -19,7 +19,7 @@ class CategoryControllerUnitTest extends TestCase
         $mockRequest->shouldReceive('get')->andReturn('teste');
 
         $mockDtoOutput = Mockery::mock(ListCategoriesOutputDto::class, [
-            [], 1, 1, 1, 1, 1, 1
+            [], 1, 1, 1, 1, 1, 1, 1
         ]);
 
         $mockUseCase = Mockery::mock(ListCategoriesUseCase::class);
