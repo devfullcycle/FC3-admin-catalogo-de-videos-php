5b79542c12fb7f2bbb458ae37fbcd01c4c827928
Carlos Ferreira <carlos@especializati.com.br>
2022-03-17 12:49:55 +0000

    Test E2E json count

--- tests/Feature/Api/CategoryApiTest.php
+++ tests/Feature/Api/CategoryApiTest.php
@@ -17,6 +17,7 @@ class CategoryApiTest extends TestCase
         $response = $this->getJson($this->endpoint);
 
         $response->assertStatus(200);
+        $response->assertJsonCount(0, 'data');
     }
 
     public function test_list_all_categories()
@@ -37,17 +38,19 @@ class CategoryApiTest extends TestCase
                 'from'
             ]
         ]);
+        $response->assertJsonCount(15, 'data');
     }
 
     public function test_list_paginate_categories()
     {
-        Category::factory()->count(30)->create();
+        Category::factory()->count(25)->create();
 
         $response = $this->getJson("$this->endpoint?page=2");
         
         $response->assertStatus(200);
         $this->assertEquals(2, $response['meta']['current_page']);
-        $this->assertEquals(30, $response['meta']['total']);
+        $this->assertEquals(25, $response['meta']['total']);
+        $response->assertJsonCount(10, 'data');
     }
 
     public function test_list_category_notfound()
