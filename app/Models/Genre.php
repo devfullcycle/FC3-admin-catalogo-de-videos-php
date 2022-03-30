1c186fe088e473235c8d0f9753b6ad895011e7e1
Carlos Ferreira <carlos@especializati.com.br>
2022-03-30 19:31:16 +0000

    Create Model and Test

---
+++ app/Models/Genre.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Genre extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'is_active',
+        'created_at',
+    ];
+
+    public $incrementing = false;
+
+    protected $casts = [
+        'id' => 'string',
+        'is_active' => 'boolean',
+        'deleted_at' => 'datetime',
+    ];
+}
---
+++ tests/Unit/App/Models/GenreUnitTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit\App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Genre;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use PHPUnit\Framework\TestCase;
+
+class GenreUnitTest extends ModelTestCase
+{
+    protected function model(): Model
+    {
+        return new Genre();
+    }
+
+    protected function traits(): array
+    {
+        return [
+            HasFactory::class,
+            SoftDeletes::class,
+        ];
+    }
+    
+    protected function fillables(): array
+    {
+        return [
+            'id',
+            'name',
+            'is_active',
+            'created_at',
+        ];
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'id' => 'string',
+            'is_active' => 'boolean',
+            'deleted_at' => 'datetime'
+        ];
+    }
+}
