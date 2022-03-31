e11ec827042eae9161e4746655893300305c9596
Carlos Ferreira <carlos@especializati.com.br>
2022-03-31 17:54:34 +0000

    Test Exception Transaction

--- tests/Feature/Core/UseCase/Genre/CreateGenreUseCaseTest.php
+++ tests/Feature/Core/UseCase/Genre/CreateGenreUseCaseTest.php
@@ -70,4 +70,38 @@ class CreateGenreUseCaseTest extends TestCase
             )
         );
     }
+
+    public function testTransactionInsert()
+    {
+        $repository = new GenreEloquentRepository(new Model());
+        $repositoryCategory = new CategoryEloquentRepository(new ModelCategory());
+
+        $useCase = new CreateGenreUseCase(
+            $repository,
+            new DBTransaction(),
+            $repositoryCategory
+        );
+
+        $categories = ModelCategory::factory()->count(10)->create();
+        $categoriesIds = $categories->pluck('id')->toArray();
+
+        try {
+            $useCase->execute(
+                new GenreCreateInputDto(
+                    name: 'teste',
+                    categoriesId: $categoriesIds
+                )
+            );
+
+            $this->assertDatabaseHas('genres', [
+                'name' => 'teste'
+            ]);
+    
+            $this->assertDatabaseCount('category_genre', 10);
+        } catch (\Throwable $th) {
+            //throw $th;
+            $this->assertDatabaseCount('genres', 0);
+            $this->assertDatabaseCount('category_genre', 0);
+        }
+    }
 }
