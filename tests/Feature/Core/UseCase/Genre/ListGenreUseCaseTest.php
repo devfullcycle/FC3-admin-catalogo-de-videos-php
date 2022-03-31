4ccce168dd08aedbdc1541d43a66fefe580e47ab
Carlos Ferreira <carlos@especializati.com.br>
2022-03-31 18:02:06 +0000

    Test Integration List Genre By ID

---
+++ tests/Feature/Core/UseCase/Genre/ListGenreUseCaseTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Genre;
+
+use App\Models\Genre as Model;
+use App\Repositories\Eloquent\GenreEloquentRepository;
+use Core\UseCase\DTO\Genre\GenreInputDto;
+use Core\UseCase\Genre\ListGenreUseCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ListGenreUseCaseTest extends TestCase
+{
+    public function testFindById()
+    {
+        $useCase = new ListGenreUseCase(
+            new GenreEloquentRepository(new Model())
+        );
+
+        $genre = Model::factory()->create();
+
+        $responseUseCase = $useCase->execute(new GenreInputDto(
+            id: $genre->id
+        ));
+
+        $this->assertEquals($genre->id, $responseUseCase->id);
+        $this->assertEquals($genre->name, $responseUseCase->name);
+    }
+}
