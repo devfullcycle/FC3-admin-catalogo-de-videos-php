9318a11d1d65487bdf07b240e35fd98c0ba01e2a
Carlos Ferreira <carlos@especializati.com.br>
2022-04-06 18:49:34 +0000

    Define Api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\{
+    CastMemberController,
     CategoryController,
     GenreController,
 };
@@ -11,6 +12,7 @@ Route::apiResource(
     name: '/genres',
     controller: GenreController::class
 );
+Route::apiResource('/cast_members', CastMemberController::class);
 
 Route::get('/', function () {
     return response()->json(['message' => 'success']);
