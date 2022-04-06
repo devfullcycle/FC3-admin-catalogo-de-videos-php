bd3fd105b1ddee9fbbe5c2e541b8a45fb4dd8877
Carlos Ferreira <carlos@especializati.com.br>
2022-04-06 18:27:44 +0000

    Create Test Controller

---
+++ tests/Feature/App/Http/Controllers/Api/CastMemberControllerTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers\Api;
+
+use App\Http\Requests\{
+    StoreCastMemberRequest,
+    UpdateCastMemberRequest,
+};
+use App\Http\Controllers\Api\CastMemberController;
+use App\Models\CastMember;
+use App\Repositories\Eloquent\CastMemberEloquentRepository;
+use Core\UseCase\CastMember\{
+    CreateCastMemberUseCase,
+    DeleteCastMemberUseCase,
+    ListCastMembersUseCase,
+    ListCastMemberUseCase,
+    UpdateCastMemberUseCase
+};
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Tests\TestCase;
+
+class CastMemberControllerTest extends TestCase
+{
+    protected $repository;
+
+    protected $controller;
+
+    protected function setUp(): void
+    {
+        $this->repository = new CastMemberEloquentRepository(
+            new CastMember()
+        );
+        $this->controller = new CastMemberController();
+
+        parent::setUp();
+    }
+
+    public function test_index()
+    {
+        $useCase = new ListCastMembersUseCase($this->repository);
+
+        $response = $this->controller->index(new Request(), $useCase);
+
+        $this->assertInstanceOf(AnonymousResourceCollection::class, $response);
+        $this->assertArrayHasKey('meta', $response->additional);
+    }
+
+    public function test_store()
+    {
+        $useCase = new CreateCastMemberUseCase($this->repository);
+
+        $request = new StoreCastMemberRequest();
+        $request->headers->set('content-type', 'application/json');
+        $request->setJson(new ParameterBag([
+            'name' => 'Teste',
+            'type' => 1
+        ]));
+
+        $response = $this->controller->store($request, $useCase);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_CREATED, $response->status());
+    }
+
+    public function test_show()
+    {
+        $castMember = CastMember::factory()->create();
+
+        $response = $this->controller->show(
+            useCase: new ListCastMemberUseCase($this->repository),
+            id: $castMember->id,
+        );
+        
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_OK, $response->status());
+    }
+
+    public function test_update()
+    {
+        $castMember = CastMember::factory()->create();
+
+        $request = new UpdateCastMemberRequest();
+        $request->headers->set('content-type', 'application/json');
+        $request->setJson(new ParameterBag([
+            'name' => 'Updated',
+            'type' => 2
+        ]));
+
+        $response = $this->controller->update(
+            request: $request,
+            useCase: new UpdateCastMemberUseCase($this->repository),
+            id: $castMember->id
+        );
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_OK, $response->status());
+        $this->assertDatabaseHas('cast_members', [
+            'name' => 'Updated'
+        ]);
+    }
+
+    public function test_delete()
+    {
+        $castMember = CastMember::factory()->create();
+
+        $response = $this->controller->destroy(
+            useCase: new DeleteCastMemberUseCase($this->repository),
+            id: $castMember->id
+        );
+
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $response->status());
+    }
+}
