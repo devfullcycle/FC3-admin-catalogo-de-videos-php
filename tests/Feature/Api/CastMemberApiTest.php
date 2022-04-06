d314b950a2d65266c7d7c4b0be5c0ce36ae4105a
Carlos Ferreira <carlos@especializati.com.br>
2022-04-06 19:41:25 +0000

    Tests E2E Pagination Cast Members

---
+++ tests/Feature/Api/CastMemberApiTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\CastMember;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class CastMemberApiTest extends TestCase
+{
+    private $endpoint = '/api/cast_members';
+
+    public function test_get_all_empty()
+    {
+        $response = $this->getJson($this->endpoint);
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonCount(0, 'data');
+    }
+
+    public function test_pagination()
+    {
+        CastMember::factory()->count(50)->create();
+
+        $response = $this->getJson($this->endpoint);
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonCount(15, 'data');
+        $response->assertJsonStructure([
+            'meta' => [
+                'total',
+                'current_page',
+                'last_page',
+                'first_page',
+                'per_page',
+                'to',
+                'from',
+            ]
+        ]);
+    }
+
+    public function test_pagination_page_two()
+    {
+        CastMember::factory()->count(20)->create();
+
+        $response = $this->getJson("$this->endpoint?page=2");
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonCount(5, 'data');
+        
+        $this->assertEquals(20, $response['meta']['total']);
+        $this->assertEquals(2, $response['meta']['current_page']);
+    }
+}
