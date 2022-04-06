c2cdabadd0abb8b2e49556b7ada6b6eb05af2838
Carlos Ferreira <carlos@especializati.com.br>
2022-04-06 18:40:31 +0000

    Bing Singleton Repository Cast Member

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,13 @@
 namespace App\Providers;
 
 use App\Repositories\Eloquent\{
+    CastMemberEloquentRepository,
     CategoryEloquentRepository,
     GenreEloquentRepository
 };
 use App\Repositories\Transaction\DBTransaction;
 use Core\Domain\Repository\{
+    CastMemberRepositoryInterface,
     CategoryRepositoryInterface,
     GenreRepositoryInterface
 };
@@ -30,6 +32,10 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(
             GenreRepositoryInterface::class,
             GenreEloquentRepository::class
+        );
+        $this->app->singleton(
+            CastMemberRepositoryInterface::class,
+            CastMemberEloquentRepository::class
         );
 
         /**
