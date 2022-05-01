c2067d637adbded149bc7450fc4514de711f4182
Carlos Ferreira <carlos@especializati.com.br>
2022-05-01 18:29:37 +0000

    Validation With Package + Notification Pattern

--- src/Core/Domain/Factory/VideoValidatorFactory.php
+++ src/Core/Domain/Factory/VideoValidatorFactory.php
@@ -4,11 +4,13 @@ namespace Core\Domain\Factory;
 
 use Core\Domain\Validation\ValidatorInterface;
 use Core\Domain\Validation\VideoLaravelValidator;
+use Core\Domain\Validation\VideoRakitValidator;
 
 class VideoValidatorFactory
 {
     public static function create(): ValidatorInterface
     {
-        return new VideoLaravelValidator();
+        // return new VideoLaravelValidator();
+        return new VideoRakitValidator();
     }
 }
--- src/Core/Domain/Validation/VideoRakitValidator.php
+++ src/Core/Domain/Validation/VideoRakitValidator.php
@@ -3,26 +3,26 @@
 namespace Core\Domain\Validation;
 
 use Core\Domain\Entity\Entity;
-use Illuminate\Support\Facades\Validator;
+use Rakit\Validation\Validator;
 
-class VideoLaravelValidator implements ValidatorInterface
+class VideoRakitValidator implements ValidatorInterface
 {
     public function validate(Entity $entity): void
     {
         $data = $this->convertEntityForArray($entity);
 
-        $validator = Validator::make($data, [
+        $validation = (new Validator())->validate($data, [
             'title' => 'required|min:3|max:255',
             'description' => 'required|min:3|max:255',
             'yearLaunched' => 'required|integer',
             'duration' => 'required|integer',
         ]);
 
-        if ($validator->fails()) {
-            foreach ($validator->errors()->messages() as $error) {
+        if ($validation->fails()) {
+            foreach ($validation->errors()->all() as $error) {
                 $entity->notification->addError([
                     'context' => 'video',
-                    'message' => $error[0]
+                    'message' => $error
                 ]);
             }
         }
