6070f47dc7f114465d40d6a035b5075b87257fca
Carlos Ferreira <carlos@especializati.com.br>
2022-05-12 21:33:09 +0000

    Define method exec

--- src/Core/UseCase/Video/Create/CreateVideoUseCase.php
+++ src/Core/UseCase/Video/Create/CreateVideoUseCase.php
@@ -2,6 +2,8 @@
 
 namespace Core\UseCase\Video\Create;
 
+use Core\Domain\Entity\Video as Entity;
+use Core\Domain\Events\VideoCreatedEvent;
 use Core\Domain\Repository\VideoRepositoryInterface;
 use Core\UseCase\Interfaces\{
     FileStorageInterface,
@@ -14,6 +16,7 @@ use Core\UseCase\Video\Create\DTO\{
 use Core\UseCase\Video\Interfaces\{
     VideoEventManagerInterface
 };
+use Throwable;
 
 class CreateVideoUseCase
 {
@@ -26,6 +29,64 @@ class CreateVideoUseCase
 
     public function exec(CreateInputVideoDTO $input): CreateOutputVideoDTO
     {
-        return new CreateOutputVideoDTO();
+        $entity = $this->createEntity($input);
+
+        try {
+            $this->repository->insert($entity);
+
+            if ($pathMedia = $this->storeMidea($entity->id(), $input->videoFile)) {
+                $this->eventManager->dispatch(new VideoCreatedEvent($entity));
+            }
+
+            $this->transaction->commit();
+
+            return new CreateOutputVideoDTO();
+        } catch (Throwable $th) {
+            $this->transaction->rollback();
+
+            throw $th;
+        }
+    }
+
+    private function createEntity(CreateInputVideoDTO $input): Entity
+    {
+        // create entity -> $input
+        $entity = new Entity(
+            title: $input->title,
+            description: $input->description,
+            yearLaunched: $input->yearLaunched,
+            duration: $input->duration,
+            opened: true,
+            rating: $input->rating,
+        );
+
+        // add categories_ids in entity - validate
+        foreach ($input->categories as $categoryId) {
+            $entity->addCategoryId($categoryId);
+        }
+
+        // add genres_ids in entity - validate
+        foreach ($input->genres as $genreId) {
+            $entity->addGenre($genreId);
+        }
+
+        // add cast_members_ids in entity - validate
+        foreach ($input->castMembers as $castMemberId) {
+            $entity->addCastMember($castMemberId);
+        }
+
+        return $entity;
+    }
+
+    private function storeMidea(string $path, ?array $media = null): string
+    {
+        if ($media) {
+            return $this->storage->store(
+                path: $path,
+                file: $media,
+            );
+        }
+
+        return '';
     }
 }
