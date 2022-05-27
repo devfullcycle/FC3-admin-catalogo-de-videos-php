d9122dd07923eee85d1b91be72dfc01677c52503
Carlos Ferreira <carlos@especializati.com.br>
2022-05-27 19:38:20 +0000

    Builder Video Interface

---
+++ src/Core/UseCase/Video/Builder/Builder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Core\UseCase\Video\Builder;
+
+use Core\Domain\Entity\Entity;
+use Core\Domain\Enum\MediaStatus;
+
+interface Builder
+{
+    public function createEntity(object $input): void;
+    public function addMediaVideo(string $path, MediaStatus $mediaStatus): void;
+    public function addTrailer(string $path): void;
+    public function addThumb(string $path): void;
+    public function addThumbHalf(string $path): void;
+    public function addBanner(string $path): void;
+    public function getEntity(): Entity;
+}
