f6cedc3e60216ee224438eb3762d68cb085bae6d
Carlos Ferreira <carlos@especializati.com.br>
2022-06-10 18:10:06 +0000

    UseCase Video DTO: Input and Output

--- src/Core/UseCase/Video/Update/DTO/UpdateInputVideoDTO.php
+++ src/Core/UseCase/Video/Update/DTO/UpdateInputVideoDTO.php
@@ -5,6 +5,16 @@ namespace Core\UseCase\Video\Update\DTO;
 class UpdateInputVideoDTO
 {
     public function __construct(
-
+        public string $id,
+        public string $title,
+        public string $description,
+        public array $categories,
+        public array $genres,
+        public array $castMembers,
+        public ?array $videoFile = null,
+        public ?array $trailerFile = null,
+        public ?array $thumbFile = null,
+        public ?array $thumbHalf = null,
+        public ?array $bannerFile = null,
     ) {}
 }
--- src/Core/UseCase/Video/Update/DTO/UpdateOutputVideoDTO.php
+++ src/Core/UseCase/Video/Update/DTO/UpdateOutputVideoDTO.php
@@ -2,9 +2,25 @@
 
 namespace Core\UseCase\Video\Update\DTO;
 
+use Core\Domain\Enum\Rating;
+
 class UpdateOutputVideoDTO
 {
     public function __construct(
-
+        public string $id,
+        public string $title,
+        public string $description,
+        public int $yearLaunched,
+        public int $duration,
+        public bool $opened,
+        public Rating $rating,
+        public array $categories = [],
+        public array $genres = [],
+        public array $castMembers = [],
+        public ?string $videoFile = null,
+        public ?string $trailerFile = null,
+        public ?string $thumbFile = null,
+        public ?string $thumbHalf = null,
+        public ?string $bannerFile = null,
     ) {}
 }
