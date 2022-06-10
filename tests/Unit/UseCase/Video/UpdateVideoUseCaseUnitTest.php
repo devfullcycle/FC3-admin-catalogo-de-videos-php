7558a4dc7ba2646c3ffb86a9e85670f9de194e65
Carlos Ferreira <carlos@especializati.com.br>
2022-06-10 17:52:40 +0000

    Unit Test Update Video UseCase

---
+++ tests/Unit/UseCase/Video/UpdateVideoUseCaseUnitTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Unit\UseCase\Video;
+
+use Core\Domain\Enum\Rating;
+use Core\Domain\ValueObject\Uuid;
+use Core\UseCase\Video\Create\UpdateVideoUseCase;
+use Core\UseCase\Video\Update\DTO\{
+    UpdateInputVideoDTO,
+    UpdateOutputVideoDTO
+};
+use Mockery;
+
+class UpdateVideoUseCaseUnitTest extends BaseVideoUseCaseUnitTest
+{
+    public function test_exec_input_output()
+    {
+        $this->createUseCase();
+
+        $response = $this->useCase->exec(
+            input: $this->createMockInputDTO(),
+        );
+
+        $this->assertInstanceOf(UpdateOutputVideoDTO::class, $response);
+    }
+    
+    protected function nameActionRepository(): string
+    {
+        return 'update';
+    }
+
+    protected function getUseCase(): string
+    {
+        return UpdateVideoUseCase::class;
+    }
+
+    protected function createMockInputDTO(
+        array $categoriesIds = [],
+        array $genresIds = [],
+        array $castMembersIds = [],
+        ?array $videoFile = null,
+        ?array $trailerFile = null,
+        ?array $thumbFile = null,
+        ?array $thumbHalf = null,
+        ?array $bannerFile = null,
+    ) {
+        return Mockery::mock(UpdateInputVideoDTO::class, [
+            Uuid::random(),
+            'title',
+            'desc',
+            2023,
+            12,
+            true,
+            Rating::RATE10,
+            $categoriesIds,
+            $genresIds,
+            $castMembersIds,
+            $videoFile,
+            $trailerFile,
+            $thumbFile,
+            $thumbHalf,
+            $bannerFile,
+        ]);
+    }
+}
