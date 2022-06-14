b6ae5ac978a85c843621d08ae8cf76c04b3bb5e5
Carlos Ferreira <carlos@especializati.com.br>
2022-06-14 15:31:46 +0000

    Create Video Factory

---
+++ database/factories/VideoFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Carbon\Carbon;
+use Core\Domain\Enum\Rating;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Video>
+ */
+class VideoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'id' => (string) Str::uuid(),
+            'title' => $this->faker->name(),
+            'description' => $this->faker->sentence(10),
+            'year_launched' => Carbon::make(now()->addYears(5))->format('Y'),
+            'opened' => true,
+            'rating' => Rating::L->value,
+            'duration' => 1,
+            'created_at' => now(),
+        ];
+    }
+}
