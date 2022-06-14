fee75343f8ece6c52954d7044eea9cb7271315c7
Carlos Ferreira <carlos@especializati.com.br>
2022-06-14 14:14:52 +0000

    Create Custom Service Provider

---
+++ app/Providers/CleanArchServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Repositories\Eloquent\{
+    CastMemberEloquentRepository,
+    CategoryEloquentRepository,
+    GenreEloquentRepository
+};
+use App\Repositories\Transaction\DBTransaction;
+use Core\Domain\Repository\{
+    CastMemberRepositoryInterface,
+    CategoryRepositoryInterface,
+    GenreRepositoryInterface
+};
+use Core\UseCase\Interfaces\TransactionInterface;
+
+class CleanArchServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(
+            CategoryRepositoryInterface::class,
+            CategoryEloquentRepository::class
+        );
+        $this->app->singleton(
+            GenreRepositoryInterface::class,
+            GenreEloquentRepository::class
+        );
+        $this->app->singleton(
+            CastMemberRepositoryInterface::class,
+            CastMemberEloquentRepository::class
+        );
+
+        /**
+         * DB Transaction
+         */
+        $this->app->bind(
+            TransactionInterface::class,
+            DBTransaction::class,
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
