79569340ae17427fb1f6ab399344b5450cde3d85
Carlos Ferreira <carlos@especializati.com.br>
2022-07-29 17:50:52 +0000

    Create Base UseCase Test

---
+++ tests/Feature/Core/UseCase/Video/BaseVideoUseCase.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Video;
+
+use App\Models\{
+    CastMember,
+    Category,
+    Genre
+};
+use Core\Domain\Enum\Rating;
+use Core\Domain\Repository\CastMemberRepositoryInterface;
+use Core\Domain\Repository\CategoryRepositoryInterface;
+use Core\Domain\Repository\GenreRepositoryInterface;
+use Core\Domain\Repository\VideoRepositoryInterface;
+use Core\UseCase\Interfaces\FileStorageInterface;
+use Core\UseCase\Interfaces\TransactionInterface;
+use Core\UseCase\Video\Create\CreateVideoUseCase;
+use Core\UseCase\Video\Create\DTO\CreateInputVideoDTO;
+use Core\UseCase\Video\Interfaces\VideoEventManagerInterface;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Tests\Stubs\UploadFilesStub;
+use Tests\Stubs\VideoEventStub;
+use Tests\TestCase;
+
+abstract class BaseVideoUseCase extends TestCase
+{
+    abstract function useCase(): string;
+    abstract function inputDTO(
+        array $categories = [],
+        array $genres = [],
+        array $castMembers = [],
+        ?array $videoFile = null,
+        ?array $trailerFile = null,
+        ?array $bannerFile = null,
+        ?array $thumbFile = null,
+        ?array $thumbHalf = null,
+    ): object;
+
+    /**
+     * @dataProvider provider
+     */
+    public function test_create(
+        int $categories,
+        int $genres,
+        int $castMembers,
+        bool $withMediaVideo = false,
+        bool $withTrailer = false,
+        bool $withThumb = false,
+        bool $withThumbHalf = false,
+        bool $withBanner = false,
+    ) {
+        $useCase = new ($this->useCase())(
+            $this->app->make(VideoRepositoryInterface::class),
+            $this->app->make(TransactionInterface::class),
+            // this->app->make(FileStorageInterface::class),
+            new UploadFilesStub(),
+            // $this->app->make(VideoEventManagerInterface::class),
+            new VideoEventStub(),
+            
+            $this->app->make(CategoryRepositoryInterface::class),
+            $this->app->make(GenreRepositoryInterface::class),
+            $this->app->make(CastMemberRepositoryInterface::class)
+        );
+
+        $categoriesIds = Category::factory()->count($categories)->create()->pluck('id')->toArray();
+        $genresIds = Genre::factory()->count($genres)->create()->pluck('id')->toArray();
+        $castMembersIds = CastMember::factory()->count($castMembers)->create()->pluck('id')->toArray();
+
+        $fakeFile = UploadedFile::fake()->create('video.mp4', 1, 'video/mp4');
+        $file = [
+            'tmp_name' => $fakeFile->getPathname(),
+            'name' => $fakeFile->getFilename(),
+            'type' => $fakeFile->getMimeType(),
+            'error' => $fakeFile->getError(),
+        ];
+
+        $input = $this->inputDTO(
+            categories: $categoriesIds,
+            genres: $genresIds,
+            castMembers: $castMembersIds,
+            videoFile: $withMediaVideo ? $file : null,
+            trailerFile: $withTrailer ? $file : null,
+            bannerFile: $withBanner ? $file : null,
+            thumbFile: $withThumb ? $file : null,
+            thumbHalf: $withThumbHalf ? $file : null,
+        );
+
+        $response = $useCase->exec($input);
+
+        $this->assertEquals($input->title, $response->title);
+        $this->assertEquals($input->description, $response->description);
+        $this->assertEquals($input->yearLaunched, $response->yearLaunched);
+        $this->assertEquals($input->duration, $response->duration);
+        $this->assertEquals($input->opened, $response->opened);
+        $this->assertEquals($input->rating, $response->rating);
+
+        $this->assertCount($categories, $response->categories);
+        $this->assertEqualsCanonicalizing($input->categories, $response->categories);
+        $this->assertCount($genres, $response->genres);
+        $this->assertEqualsCanonicalizing($input->genres, $response->genres);
+        $this->assertCount($castMembers, $response->castMembers);
+        $this->assertEqualsCanonicalizing($input->castMembers, $response->castMembers);
+
+        $this->assertTrue($withMediaVideo ? $response->videoFile !== null : $response->videoFile === null);
+        $this->assertTrue($withTrailer ? $response->trailerFile !== null : $response->trailerFile === null);
+        $this->assertTrue($withBanner ? $response->bannerFile !== null : $response->bannerFile === null);
+        $this->assertTrue($withThumb ? $response->thumbFile !== null : $response->thumbFile === null);
+        $this->assertTrue($withThumbHalf ? $response->thumbHalf !== null : $response->thumbHalf === null);
+    }
+
+    protected function provider(): array
+    {
+        return [
+            'Test with all IDs and media video' => [
+                'categories' => 3,
+                'genres' => 3,
+                'castMembers' => 3,
+                'withMediaVideo' => true,
+                'withTrailer' => false,
+                'withThumb' => false,
+                'withThumbHalf' => false,
+                'withBanner' => false,
+            ], 
+            'Test with categories and genres and without files' => [
+                'categories' => 3,
+                'genres' => 3,
+                'castMembers' => 0,
+            ], 
+            'Test with all IDs and all medias' => [
+                'categories' => 2,
+                'genres' => 2,
+                'castMembers' => 2,
+                'withMediaVideo' => true,
+                'withTrailer' => true,
+                'withThumb' => true,
+                'withThumbHalf' => true,
+                'withBanner' => true,
+            ], 
+            'Test without IDs and all medias' => [
+                'categories' => 0,
+                'genres' => 0,
+                'castMembers' => 0,
+                'withMediaVideo' => true,
+                'withTrailer' => true,
+                'withThumb' => true,
+                'withThumbHalf' => true,
+                'withBanner' => true,
+            ], 
+        ];
+    }
+}
