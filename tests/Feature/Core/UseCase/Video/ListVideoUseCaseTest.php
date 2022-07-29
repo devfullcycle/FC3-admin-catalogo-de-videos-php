6230f21eb4cea05f1be23de2a97add47aec4ca52
Carlos Ferreira <carlos@especializati.com.br>
2022-07-29 18:25:31 +0000

    Integration Test - List Video UseCase

---
+++ tests/Feature/Core/UseCase/Video/ListVideoUseCaseTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Video;
+
+use App\Models\Video;
+use Core\Domain\Exception\NotFoundException;
+use Core\Domain\Repository\VideoRepositoryInterface;
+use Core\UseCase\Video\List\DTO\ListInputVideoUseCase;
+use Core\UseCase\Video\List\ListVideoUseCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ListVideoUseCaseTest extends TestCase
+{
+    public function test_list()
+    {
+        $video = Video::factory()->create();
+
+        $useCase = new ListVideoUseCase(
+            $this->app->make(VideoRepositoryInterface::class)
+        );
+
+        $response = $useCase->exec(new ListInputVideoUseCase(
+            id: $video->id
+        ));
+
+        $this->assertNotNull($response);
+        $this->assertEquals($video->id, $response->id);
+    }
+
+    public function test_exception()
+    {
+        $this->expectException(NotFoundException::class);
+
+        $useCase = new ListVideoUseCase(
+            $this->app->make(VideoRepositoryInterface::class)
+        );
+
+        $useCase->exec(new ListInputVideoUseCase(
+            id: 'fake_id'
+        ));
+    }
+}
