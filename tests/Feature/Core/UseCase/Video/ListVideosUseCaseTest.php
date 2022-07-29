3888686ad34dea6ad7f3ed8a0c0a119f3665f377
Carlos Ferreira <carlos@especializati.com.br>
2022-07-29 18:39:55 +0000

    Integration Test - Pagination UseCase Video

---
+++ tests/Feature/Core/UseCase/Video/ListVideosUseCaseTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Video;
+
+use App\Models\Video;
+use Core\Domain\Repository\VideoRepositoryInterface;
+use Core\UseCase\Video\Paginate\DTO\PaginateInputVideoDTO;
+use Core\UseCase\Video\Paginate\ListVideosUseCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ListVideosUseCaseTest extends TestCase
+{
+    /**
+     * @dataProvider provider
+     */
+    public function test_pagination(
+        int $total,
+        int $perPage,
+    ) {
+        Video::factory()->count($total)->create();
+
+        $useCase = new ListVideosUseCase(
+            $this->app->make(VideoRepositoryInterface::class)
+        );
+
+        $response = $useCase->exec(new PaginateInputVideoDTO(
+            filter: '',
+            order: 'desc',
+            page: 1,
+            totalPerPage: $perPage
+        ));
+
+        $this->assertCount($perPage, $response->items);
+        $this->assertEquals($total, $response->total);
+    }
+
+    protected function provider(): array
+    {
+        return [
+            [
+                'total' => 30,
+                'perPage' => 10,
+            ], [
+                'total' => 20,
+                'perPage' => 5,
+            ], [
+                'total' => 0,
+                'perPage' => 0,
+            ],
+        ];
+    }
+}
