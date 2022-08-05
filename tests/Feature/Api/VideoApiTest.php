d58fa8cf374fe0e1f5d4af4a4eaf44b9b508fef8
Carlos Ferreira <carlos@especializati.com.br>
2022-08-05 18:46:17 +0000

    test: valid json structure api

--- tests/Feature/Api/VideoApiTest.php
+++ tests/Feature/Api/VideoApiTest.php
@@ -12,20 +12,49 @@ class VideoApiTest extends TestCase
 {
     protected $endpoint = '/api/videos';
     
-    public function testEmpty()
+    /**
+     * @test
+     */
+    public function empty()
     {
         $response = $this->getJson($this->endpoint);
 
         $response->assertStatus(Response::HTTP_OK);
     }
 
-    public function testPagination()
+    /**
+     * @test
+     */
+    public function pagination()
     {
-        $videos = Video::factory()->count(20)->create();
+        Video::factory()->count(20)->create();
         
         $response = $this->getJson($this->endpoint);
         
         $response->assertStatus(Response::HTTP_OK);
         $response->assertJsonCount(15, 'data');
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id',
+                    'title',
+                    'description',
+                    'year_launched',
+                    'opened',
+                    'rating',
+                    'duration',
+                    'created_at',
+                ]
+            ], 
+            'meta' => [
+                'total',
+                'current_page',
+                'last_page',
+                'first_page',
+                'per_page',
+                'to',
+                'from',
+            ]
+        ]);
     }
 }
