78da2b149f962b3ff7d0e8ab6f19ef461a3f7cda
Carlos Ferreira <carlos@especializati.com.br>
2022-09-02 20:39:18 +0000

    feat: Send Video to Microservice Encoder

--- app/Listeners/SendVideoToMicroEncoder.php
+++ app/Listeners/SendVideoToMicroEncoder.php
@@ -2,6 +2,7 @@
 
 namespace App\Listeners;
 
+use App\Services\AMQP\AMQPInterface;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
@@ -13,10 +14,9 @@ class SendVideoToMicroEncoder
      *
      * @return void
      */
-    public function __construct()
-    {
-        //
-    }
+    public function __construct(
+        private AMQPInterface $amqp
+    ) {}
 
     /**
      * Handle the event.
@@ -26,6 +26,9 @@ class SendVideoToMicroEncoder
      */
     public function handle($event)
     {
-        Log::info($event->getPayload());
+        $this->amqp->producerFanout(
+            payload: $event->getPayload(),
+            exchange: config('microservices.micro_encoder_go.exchange')
+        );
     }
 }
