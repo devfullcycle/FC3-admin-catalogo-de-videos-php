8f899f38bfd7770eaf6b61874da730a518bc9782
Carlos Ferreira <carlos@especializati.com.br>
2022-09-02 20:14:30 +0000

    feat: connection amqp service

--- app/Services/AMQP/PhpAmqpService.php
+++ app/Services/AMQP/PhpAmqpService.php
@@ -3,9 +3,31 @@
 namespace App\Services\AMQP;
 
 use Closure;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 class PhpAmqpService implements AMQPInterface
 {
+    protected $connection = null;
+    protected $channel = null;
+
+    public function __construct()
+    {
+        if ($this->connection) {
+            return;
+        }
+
+        $configs = config('microservices.rabbitmq.hosts')[0];
+        $this->connection = new AMQPStreamConnection(
+            host: $configs['host'],
+            port: $configs['port'],
+            user: $configs['user'],
+            password: $configs['password'],
+            vhost: $configs['vhost']
+        );
+
+        $this->channel = $this->connection->channel();
+    }
+
     public function producer(string $queue, array $payload, string $exchange): void
     {
 
@@ -13,7 +35,7 @@ class PhpAmqpService implements AMQPInterface
 
     public function producerFanout(string $queue, array $payload, string $exchange): void
     {
-
+        
     }
 
     public function consumer(string $queue, string $exchange, Closure $callback): void
