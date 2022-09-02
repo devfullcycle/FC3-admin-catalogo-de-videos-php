06c2e2523134715404a46ef5f4df7e189b0df059
Carlos Ferreira <carlos@especializati.com.br>
2022-09-02 18:36:14 +0000

    test: integration test - change encoded path video

---
+++ tests/Feature/Core/UseCase/Video/ChangeEncodedPathVideoTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Video;
+
+use App\Models\Video as Model;
+use Core\Domain\Repository\VideoRepositoryInterface;
+use Core\UseCase\Video\ChangeEncoded\ChangeEncodedPathVideo;
+use Core\UseCase\Video\ChangeEncoded\DTO\ChangeEncodedVideoDTO;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ChangeEncodedPathVideoTest extends TestCase
+{
+    public function testIfUpdatedMediaInDatabase()
+    {
+        $video = Model::factory()->create();
+
+        $useCase = new ChangeEncodedPathVideo(
+            $this->app->make(VideoRepositoryInterface::class)
+        );
+
+        $input = new ChangeEncodedVideoDTO(
+            id: $video->id,
+            encodedPath: 'path-id/video_encoded.ext',
+        );
+
+        $useCase->exec($input);
+
+        $this->assertDatabaseHas('medias_video', [
+            'video_id' => $input->id,
+            'encoded_path' => $input->encodedPath
+        ]);
+    }
+}
